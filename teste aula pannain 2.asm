3e868356efc725833f710d2b30b0c4c064d3b38b
Maria Elisa <mariaelisaf26@gmail.com>
2024-11-14 14:26:34 -0300

    atualização

--- teste aula pannain 2.asm
+++ teste aula pannain 2.asm
@@ -1,32 +1,45 @@
 .MODEL SMALL
 .STACK 100H
+
 PULA_LINHA MACRO  
 
-MOV AH,2                 
-MOV DL,13  
-INT 21H 
-MOV DL,10 
-INT 21H 
+    MOV AH,2                 
+    MOV DL,10 
+    INT 21H 
 
 ENDM
 
 IMPRIMIR MACRO MSG
 
- MOV AH,9
- LEA DX,MSG
- INT 21H
+    MOV AH,9
+    LEA DX,MSG
+    INT 21H
 
 ENDM
 
 ESPAÇO MACRO
- MOV AH,2
- MOV DL,' '
- INT 21H
+    PUSH AX
+    PUSH DX
+    MOV AH,2
+    MOV DL,' '
+    INT 21H
+    POP DX
+    POP AX
+
+ENDM
+
+TIPO MACRO
+    STD 
+    LEA SI,NUMJOGO+8h 
+    LEA DI,NUMJOGO+0Ah  
+    MOV CX,AL
+    REP MOVSW 
+    MOV WORD PTR [DI],3
 ENDM
 
 .DATA 
 
- JOGO0  DB     10 DUP('o')                   ; Linha vazia
+JOGO0   DB     10 DUP('o')                   ; Linha vazia
         DB     10 DUP('o')                   ; Linha vazia
         DB     10 DUP('o')                   ; Linha vazia
         DB     10 DUP('o')                   ; Linha vazia
@@ -40,7 +53,7 @@ ENDM
 
 JOGO1   DB     10 DUP ('.')                                              ;LINHA VAZIA
         DB     3  DUP ('.'), 4 DUP ('#'), 2  DUP ('.'), '#'              ;LINHA COM ENCOURAÇADO(HORIZONTAL) E PARTE DO SUBMARINO 1(VERTICAL)
-        DB     9  DUP ('.'), "#"                                         ;LINHA COM A OUTRA PARTE DO SUBMARINO 1(VERTICAL)
+        DB     9  DUP ('.'), '#'                                         ;LINHA COM A OUTRA PARTE DO SUBMARINO 1(VERTICAL)
         DB     '.', '#', 8 DUP('.')                                      ;LINHA COM PARTE1 DO HIDROAVIÃO 1(VERTICAL)
         DB     '.', 2 DUP ('#'), 2 DUP ('.'), 3 DUP ('#'), 2 DUP ('.')   ;LINHA COM PARTE2 DO HIDROAVIÃO 1(VERTICAL) E FRAGATA(HORIZONTAL)
         DB     '.', '#', 8 DUP ('.')                                     ;LINHA COM PARTE3 DO HIDROAVIÃO 1(VERTICAL)
@@ -49,10 +62,61 @@ JOGO1   DB     10 DUP ('.')                                              ;LINHA 
         DB     2  DUP ('.'), 2 DUP ('#'), 3 DUP ('.'), '#', 2 DUP ('.')  ;LINHA COM SUBMARINO 2(HORIZONTAL) E PARTE2 DO HIDROAVIÃO 2(HORIZONTAL)
         DB     10 DUP ('.')                                              ;LINHA VAZIA
 
+JOGO2   DB     2 DUP ('#'), 5 DUP('.'), 3 DUP ('#')  
+        DB     8 DUP ('.'), '#', '.'
+        DB     10 DUP ('.') 
+        DB     10 DUP ('.') 
+        DB     3 DUP ('#'), 7 DUP ('.')  
+        DB     '.', '#', 4 DUP ('.'), '#', 3 DUP ('.') 
+        DB     6 DUP ('.'), '#', 2 DUP('.'), '#' 
+        DB     9 DUP ('.'), '#'
+        DB     3 DUP ('#'), 6 DUP ('.'), '#' 
+        DB     9 DUP ('.'), '#'
+
+JOGO3   DB     6 DUP ('.'), 2 DUP ('#'), 2 DUP ('.')
+        DB     3 DUP ('#'), 7 DUP ('.')
+        DB     '.','#', 7 DUP ('.'), '#'
+        DB     9 DUP ('.'), '#' 
+        DB     9 DUP ('.'), '#'   
+        DB     2 DUP ('.'), 4 DUP ('#'), 4 DUP ('.')
+        DB     10 DUP ('.')
+        DB     10 DUP ('.')
+        DB     '#', 7 DUP ('.'), '#', '.'
+        DB     '#', 6 DUP ('.'),3 DUP ('#')
+
+JOGO4   DB     5 DUP ('.'),'#', 4 DUP ('.')
+        DB     '.', 2 DUP ('#'), 2 DUP ('.'),'#', 4 DUP ('.')
+        DB     5 DUP ('.'),'#', 4 DUP ('.')
+        DB     8 DUP ('.'), '#', '.'
+        DB     7 DUP ('.'), 2 DUP ('#'), '.'  
+        DB     '#', 2 DUP ('.'), '#', 4 DUP ('.'), '#', '.'
+        DB     '#', '.', 3 DUP ('#'), 5 DUP ('.')
+        DB     '#',9 DUP ('.')
+        DB     '#', 5 DUP ('.'), 2 DUP ('#'), 2 DUP ('.')
+        DB     10 DUP ('.')
+
+JOGO5   DB     2 DUP ('#'),8 DUP ('.')
+        DB     10 DUP ('.')
+        DB     2 DUP ('.'),'#','.', 3 DUP ('#'), 3 DUP ('.')
+        DB     2 DUP ('.'),'#',2 DUP ('.'), '#', 4 DUP ('.')
+        DB     2 DUP ('.'),'#',7 DUP ('.')  
+        DB     '#','.','#',7 DUP ('.')
+        DB     '#',9 DUP ('.')
+        DB     5 DUP ('.'), '#', 4 DUP ('.')
+        DB     4 DUP ('.'), 2 DUP ('#'), 4 DUP ('.')
+        DB     3 DUP ('#'), 2 DUP ('.'), '#', 4 DUP ('.')
+
+
+
 INICIAL DB 10,13, "VAMOS JOGAR BATALHA NAVAL!!! $"
 
 INTRODUCAO DB 10,13, "PARA COMECAR O JOGO, TEMOS UM TABULEIRO DE 20 LINHAS E 20 COLUNAS QUE TERAO AS EMBARCACOES $"
+
 INSTRUCOES DB 10,13, "PARA JOGAR, EH NECESSARIO QUE SE INSIRA O VALOR DA LINHA E COLUNA DESEJADO PARA DAR O TIRO E DESCOBRIR SE FOI ATINGIDO UMA EMBARCACAO, PARA GANHAR EH NECESSARIO QUE SE ACHEM TODAS AS EMBARCACOES $"
+
+COMECO DB 10,13, "PRESSIONE (1) PARA COMECAR O JOGO OU (0) PARA FINALIZAR:  $ "
+
+INCORRETO DB 10,13, "INSIRA UM NUMERO VALIDO PARA PROSSEGUIR:$"
 
 LINHAS DB 10,13, "INSIRA A LINHA DESEJADA PARA ATIRAR: $"
 
@@ -67,19 +131,24 @@ ERROU  DB 10,13,'POXA, NAO FOI DESSA VEZ...$'
 FINAL DB 10,13,'FIM DE JOGO!$'
 
 DECO DB 10,13,'...............................$'
+
 LATERAIS DB ?
 
+OPÇÕES DB 13,10,'ESCOLHA UM TIPO DE JOGO(1-5):$'
+
+NUMJOGO DB 'J','O','G','O',?
 
 .CODE 
 
 MAIN PROC  
 
-MOV AX,@DATA
-MOV DS,AX
+    MOV AX,@DATA
+    MOV DS,AX
+    MOV ES,AX
 
 IMPRIMIR INICIAL
 
-PULA_LINHA    ; Pulando uma linha após a mensagem inicial
+PULA_LINHA    
 
 IMPRIMIR INTRODUCAO 
 
@@ -89,227 +158,287 @@ IMPRIMIR INSTRUCOES
 
 PULA_LINHA 
 
+IMPRIMIR COMECO 
+
+CALL @START
+
+PULA_LINHA
+
 CALL @IMPRIMIR
 CALL RODADAS
 
 PULA_LINHA
 
 FINALIZA:
-MOV AH,4CH
-INT 21H
+    MOV AH,4CH
+    INT 21H
 MAIN ENDP 
 
+@START PROC                                    
+
+
+VOLTAR:
+
+    MOV AH,1            ; Lê um caractere do teclado
+    INT 21H             ; AL contém o caractere digitado
+    CMP AL,'1'          ; Compara com o caractere ASCII '1' (49)
+    JE COMECA           ; Se for igual a '1', vai para COMECA
+    CMP AL,'0'          ; Compara com o caractere ASCII '0' (48)
+    JE FIM              ; Se for igual a '0', vai para FIM
+
+    ; Mensagem de erro, entrada inválida
+    IMPRIMIR INCORRETO
+    JMP VOLTAR           ; Volta para pedir nova entrada
+
+FIM:
+    IMPRIMIR FINAL
+    MOV AH,4CH          ; Finaliza o programa
+    INT 21H
+
+COMECA:
+    RET                 ; Retorna para o fluxo principal após entrada válida
+
+@START ENDP
+
+@ESCOLHA PROC
+    PUSH CX
+    IMPRIMIR OPÇÕES
+    MOV AH,1
+PULO:
+    INT 21h
+    CMP AL, '1'        ; Verifica se o caractere é um dígito
+    JGE NUM
+    CMP AL, '5'
+    JLE NUM
+    IMPRIMIR INCORRETO
+    JMP PULO
+NUM:
+    
+    POP CX
+    RET
+@ESCOLHA ENDP
+
 @COORDENADAS PROC
- XOR BX,BX
- MOV CX,CONSTANTE
- PULA_LINHA
- ESPAÇO
- ESPAÇO
- ESPAÇO
+    PUSH CX
+    XOR BX,BX
+    MOV CX,CONSTANTE
+    PULA_LINHA
+    ESPAÇO
+    ESPAÇO
+    ESPAÇO
+
 @DECO:
- MOV AX,BX
- CALL SAIDEC
- CMP AX,9
- JLE PULODUP
- ESPAÇO
- JMP CONTINUA
+
+    MOV AX,BX
+    CALL SAIDEC
+    CMP AX,9
+    JLE PULODUP
+    ESPAÇO
+    JMP CONTINUA
+
 PULODUP:
- ESPAÇO
- ESPAÇO
+
+    ESPAÇO
+    ESPAÇO
+
 CONTINUA:
- INC BX
- LOOP @DECO
- RET
+
+    INC BX
+    LOOP @DECO
+    POP CX
+    RET
 @COORDENADAS ENDP
 
 @IMPRIMIR PROC
- CALL @COORDENADAS
- IMPRIMIR DECO
+    CALL @COORDENADAS
+    IMPRIMIR DECO
 
- PUSH CX
- XOR BX,BX
- XOR DL,DL
- MOV DI,CONSTANTE
- PULA_LINHA
- mov LATERAIS,41h
- IMPRIME2:
- MOV AH,2
- MOV DL,LATERAIS
- INT 21H
- ADD LATERAIS,1
- MOV AH,2
- MOV DL,'.'
- INT 21H
- ESPAÇO
- XOR SI,SI
- MOV CX,CONSTANTE
- MOV AH,2
- IMPRIME:
- MOV DL,JOGO0[BX+SI]
- INT 21H
- ESPAÇO
- ESPAÇO
- INC SI
- LOOP IMPRIME
- push dx
- PULA_LINHA
- pop dx
- ADD BX,CONSTANTE
- DEC DI
- JNZ IMPRIME2
- POP CX
- RET
+    PUSH CX
+    XOR BX,BX
+    XOR DL,DL
+    MOV DI,CONSTANTE
+    PULA_LINHA
+    mov LATERAIS,41h
+    IMPRIME2:
+    MOV AH,2
+    MOV DL,LATERAIS
+    INT 21H
+    ADD LATERAIS,1
+    MOV AH,2
+    MOV DL,'.'
+    INT 21H
+    ESPAÇO
+    XOR SI,SI
+    MOV CX,CONSTANTE
+    MOV AH,2
+
+IMPRIME:
+
+    MOV DL,JOGO0[BX+SI]
+    INT 21H
+    ESPAÇO
+    ESPAÇO
+    INC SI
+    LOOP IMPRIME
+    PUSH DX
+    PULA_LINHA
+    POP DX
+    ADD BX,CONSTANTE
+    DEC DI
+    JNZ IMPRIME2
+    POP CX
+    RET
 @IMPRIMIR ENDP
 
 RODADAS PROC
- MOV CX,CONSTANTE
- RODADA:
-  IMPRIMIR LINHAS
-   MOV AH,1
-   INT 21H
-   XOR AH,AH
-   SUB AL,65D
-   MOV BX,AX
-   
-PROX: 
+
+    MOV CX,2
+
+RODADA:
+IMPRIMIR LINHAS
+    MOV AH,1
+    INT 21H
+    XOR AH,AH
+    SUB AL,65D
+    MOV BX,AX
+    MOV AX, 10
+    MUL BX 
+    MOV BX,AX
+
+PROX:
+
 PULA_LINHA 
 
-IMPRIMIR COLUNAS
-CALL ENTDEC
-XOR AH,AH  
-MOV SI ,AX
+    IMPRIMIR COLUNAS
+    CALL ENTDEC
+    XOR AH,AH  
+    MOV SI ,AX
 
 PULA_LINHA 
 
 COMPARA:
- CMP JOGO1[BX+SI],'#'
- JNE VAZIO
- MOV JOGO0[BX+SI],'#'
- CALL @IMPRIMIR
- IMPRIMIR ACERTOU
- DEC CX
- CMP CX,'0'
- JZ SALTA
- JMP RODADA
 
- VAZIO:
- MOV JOGO0[BX+SI],'.'
- CALL @IMPRIMIR
- IMPRIMIR ERROU
- DEC CX
- CMP CX,'0'
- JZ SALTA
- JMP RODADA
+    CMP JOGO1[BX+SI],'#' ;mudar o numero da palavra
+    JNE VAZIO
+    MOV JOGO0[BX+SI],'#'
+    CALL @IMPRIMIR
+    IMPRIMIR ACERTOU
+    DEC CX
+    CMP CX,0
+    JZ SALTA
+    JMP RODADA
+
+VAZIO:
+    MOV JOGO0[BX+SI],'.'
+    CALL @IMPRIMIR
+    IMPRIMIR ERROU
+    DEC CX
+    CMP CX,0
+    JZ SALTA
+    JMP RODADA 
+     
 SALTA:
- RET
+    IMPRIMIR FINAL
+    RET
+
 RODADAS ENDP
 
 ENTDEC PROC
- PUSH CX
- PUSH BX
- PUSH DX
+    PUSH CX
+    PUSH BX
+    PUSH DX
 ENT:
+    ; Limpa os registradores BX e CX
+    XOR BX, BX
+    XOR CX, CX
 
- XOR BX,BX
- XOR CX,CX
+    ; Leitura do primeiro caractere
+    MOV AH, 1
+    INT 21h
 
- MOV AH,1
- INT 21H
-
- CMP AL,'-'
- JE NEGT
- CMP AL,'+'
- JE POST
- JMP REP
-NEGT:
- MOV CX,1
-POST:
- INT 21H
 REP:
- CMP AL,'0'
- JNGE NUM
- CMP AL,'9'
- JNLE NUM
- AND AX,000FH
- PUSH AX
+    CMP AL, '0'        ; Verifica se o caractere é um dígito
+    JNGE NUM
+    CMP AL, '9'
+    JNLE NUM
+    AND AX, 000Fh      ; Converte o caractere em valor numérico
+    PUSH AX
 
- MOV AX,10
- MUL BX
- POP BX
- ADD BX,AX
+    MOV AX, 10
+    MUL BX             ; Multiplica BX por 10
+    POP BX
+    ADD BX, AX         ; Adiciona o dígito convertido
 
- MOV AH,1
- INT 21H
- CMP AL,13
- JNE REP
+    MOV AH, 1
+    INT 21h
+    CMP AL, 13         ; Verifica se Enter foi pressionado
+    JNE REP
 
- MOV AX,BX
- OR CX,CX
- JE SAI
- NEG AX
- JMP SAI
+    MOV AX, BX
+    OR CX, CX
+    JE SAI
 NUM:
- MOV AH,2
- MOV DL,0DH
- INT 21H
- MOV DL,0AH
- INT 21H
- JMP ENT
+    MOV AH, 2
+    MOV DL, 0Dh        ; Imprime uma nova linha
+    INT 21h
+    MOV DL, 0Ah
+    INT 21h
+    JMP ENT
 SAI:
- POP DX
- POP BX
- POP CX
- RET
- ENTDEC ENDP
+    POP DX
+    POP BX
+    POP CX
+    RET       
 
- SAIDEC PROC
-; imprime numero decimal sinalizado em AX
-; entrada AX
-; saida nenhuma
+ENTDEC ENDP
+
+SAIDEC PROC
+
     PUSH AX
     PUSH BX
     PUSH CX
     PUSH DX
-; if AX < 0
-    OR AX,AX      ; AX < 0 ?
+
+    OR AX,AX      
     JGE @END_IF1
-;then
-    PUSH AX     ;salva o numero
+
+    PUSH AX     
     MOV DL, '-'
     MOV AH,2
-    INT 21H         ; imprime -
-    POP AX          ; restaura numero
+    INT 21H      
+    POP AX          
     NEG AX
 
-; digitos decimais
 @END_IF1:
-    XOR CX,CX      ; contador de d?gitos
-    MOV BX,10      ; divisor
+
+    XOR CX,CX      
+    MOV BX,10
+    
 @REP1:
-    XOR DX,DX      ; prepara parte alta do dividendo
-    DIV BX         ; AX = quociente   DX = resto
-    PUSH DX        ; salva resto na pilha
-    INC CX         ; contador = contador +1
 
-;until
-    OR AX,AX       ; quociente = 0?
-    JNE @REP1      ; nao, continua
+    XOR DX,DX     
+    DIV BX         
+    PUSH DX      
+    INC CX        
 
-; converte digito em caractere
+
+    OR AX,AX       
+    JNE @REP1    
     MOV AH,2
 
-; for contador vezes
 @IMP_LOOP:
-    POP DX        ; digito em DL
+
+    POP DX        
     OR DL,30H
     INT 21H
     LOOP @IMP_LOOP
-; fim do for
+
 
     POP DX
     POP CX
     POP BX
     POP AX
     RET
+
 SAIDEC ENDP
 END MAIN
